5301d0fceebb11c2e792372d8052dc0aa83693d6
belalschenwari <belalschenwari@gmail.com>
2021-02-05 15:16:12 +0100

    FileReader ausgelagert in eigenständige Funktion; MEthode readBlacklist erstellt;

--- webCrawler/src/at/webCrawler/Main.java
+++ webCrawler/src/at/webCrawler/Main.java
@@ -9,16 +9,12 @@ import com.gargoylesoftware.htmlunit.html.DomNode;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import org.w3c.dom.Node;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.text.NumberFormat;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
     //TODO: java.sql.* anpassen auf benötigte SQL imports und diese einzeln importieren
@@ -245,9 +241,15 @@ public class Main {
      */
     public static void registerKeywords(String textForKeywords, int relevanz, HashMap<String, Integer> keywords) {
         //Blacklist Bindewörter
+        //TODO: Seitensprache ermitteln und entsprechende Blacklist wählen
+        /*englisch = C:\Users\DCV\Desktop\webcrawlerEnglisch.txt
+                duetsch = C:\Users\DCV\Desktop\webcrawlerDeutsch.txt
+                polnisch = C:\Users\DCV\Desktop\webcrawlerPolnisch.txt
+         String seitensprache  */
         //TODO: Blacklist aus externem Log/Dokument entsprechend Seitensprache / meta charset?
-        List<String> disabledKeywords = FileReader.blackListKeyword();
-//        String[] disabledKeywords = blackListKeyword();
+        //TODO: verschiedenen Blacklist-Pfade als Konstante hinterlegen für jede Sprache
+        List<String> disabledKeywords = FileReader.readBlacklistKeyword("C:\\Users\\DCV\\Desktop\\webcrawlerDeutsch.txt");
+
         //alles was kein Schriftzeichen ist entfernen, ö.ä,ü bleiben bestehen
         textForKeywords = textForKeywords.replaceAll("[\\p{Punct}]+", " ")
                 .replaceAll("[\\p{Digit}]+", " ") //alle Zahlen ...
