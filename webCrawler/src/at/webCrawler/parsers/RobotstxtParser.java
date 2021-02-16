7482913afdbc53d7f538f0e1e8c238185dbc158f
Eishenne <Eishenne@gmx.de>
2021-02-16 16:00:36 +0100

    robots.txt analyze

--- webCrawler/src/at/webCrawler/parsers/RobotstxtParser.java
+++ webCrawler/src/at/webCrawler/parsers/RobotstxtParser.java
@@ -8,7 +8,6 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
-import java.util.List;
 import java.util.Scanner;
 
 public class RobotstxtParser {
@@ -41,47 +40,54 @@ public class RobotstxtParser {
         String robotsTxtContent = "";
 
         //robots.txt
-        //-robots.txt holen
-        //---URL zur robots.txt erzeugen
+        //-robots.txt Anfrage vorbereiten
+        //--URL zur robots.txt erzeugen
         robotsTxtUri = URI.create(base + "/robots.txt");
 //            System.out.println("robots.txt URI = " + robotsTxtUri);
 
-        //-robots.txt Anfrage beschreiben
+        //--robots.txt Anfrage beschreiben
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(robotsTxtUri)
                 .timeout(Duration.ofMinutes(2))
                 .build();
         try {
-            //robots.txt Anfrage durchführen (+Auswahl Rückgabeform)
+            //-robots.txt Anfrage durchführen (+Auswahl Rückgabeform)
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            //robots.txt Anfrage Erfolg prüfen und Ergebnis verarbeiten
+            //-robots.txt Anfrage Erfolg prüfen und Ergebnis verarbeiten
             if (response.statusCode() == HttpStatus.SC_OK) {
 //                System.out.println("Ziel :" + robotsTxtUri);
-                //robots.txt in String füllen
+                //--robots.txt in String füllen
                 robotsTxtContent = response.body();
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-
 //        System.out.println(robotsTxtContent);
         return robotsTxtContent;
     }
 
     /**
-     * splits String into separate Strings for each line
+     * Splits String into separate Strings for each line. Every line gets analyzed for its content.
+     * As long as there is no method to determine if the content is for another webcrawler,
+     * we do follow all given regulations. Which is a bad idea.
+     * TODO: search for "User-Agent: AFFE and get the following lines until there is another "User-Agent: "
+     * TODO: if there is no "User-Agent: AFFE" search for "User-Agent: *"
+     * TODO: only read lines which follow "User-Agent: * or AFFE" until another User-Agent is mentioned
      * @param robotsTxtContent robots.txt as a String
      */
     private static void splitRobotsTxt(String robotsTxtContent) {
+        String entry = "";
         Scanner scanner = new Scanner(robotsTxtContent);
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
             if (line.isEmpty()) {
                 continue;
             }
-            //Zeile analysieren / einordnen und so
+            //Zeilen lesen welche für affe und alle crawler gelten
+
+            //Zeile analysieren / einordnen
             System.out.println(line);
-            analyzeLine(line);
+            analyzeLineFormat(line);
 
         }
         scanner.close();
@@ -89,20 +95,45 @@ public class RobotstxtParser {
 
 
     /**
-     * analyzes a line of the robots.txt to check what the content wants to control
+     * Analyzes a line of the robots.txt to check what the content wants to control. It has to single out the given
+     * value of a line.
+     * Should only handle lines that are relevant for our or every crawler.
+     *
      * @param line a single line of the robots.txt
      */
-    private static void analyzeLine(String line) {
+    private static void analyzeLineFormat(String line) {
+        //unformatierter Eintrag
+        String entry = "";
+        //formatierter Eintrag zum anbinden an nextTarget
+        String formatedEntry = "";
+
+        //für welche crawler gelten die folgenden Anweisungen
+        //TODO: vorverlagern, hier sollen nur Zeilen landen die für uns gelten
+        if (line.toLowerCase().startsWith("user-agent")) {
+            //Eintrag erhalten
+            entry = line.substring(10).trim();
+            if ((entry.equalsIgnoreCase("affe")) || (entry.equalsIgnoreCase("*"))) {
+                //gilt für uns oder jeden webcrawler
+
+            }
+        }
+
+
         if (line.toLowerCase().startsWith("disallow")) {
             //Pfad welcher nicht erlaubt ist
-            String entry = line.substring(8).trim();
+            entry = line.substring(8).trim();
             //
             //Form der Pfadangabe verarbeiten
-            // */uri
-            // /*uri
-            // /*/uri
-            // /uri*
-            // /uri/*
+            // */entry
+            if (entry.startsWith("*")) {
+                entry = entry.replace("*", "");
+            }
+//            if (entry.)
+            // /*entry
+            // /*/entry
+            // /entry*
+            // /entry/*
+
 
 
         }
